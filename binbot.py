90bff7fc05a1b95d282504a9b3db08f8f90cc7a0
Karan <karan@Karans-MacBook-Air.local>
2023-07-07 20:34:44 +0530

    new commit3

--- binbot.py
+++ binbot.py
@@ -1,35 +1,39 @@
 import streamlit as st
 import pandas as pd
-from binance.client import Client
+import requests
 import base64
+from datetime import datetime
 import pytz
 
-# Binance API credentials
-API_KEY = 'Xfh7L86PTI39vFaJNdcHsNoMw5J6qu8W6el4oTsLZtRbUNadJYmCcRFF8pOHhv9f'
-API_SECRET = '3fb5dqB2GxIIKCpHpu3TCPHQmBOl8KcugdDnYvRbSImsBawoVQdRZVJtRTqBYocy'
-
-# Initialize Binance client
-client = Client(API_KEY, API_SECRET)
 
 class FuturesApp:
     def fetch_data(self, symbol, interval, limit):
-        klines = client.futures_klines(symbol=symbol, interval=interval, limit=limit)
-        df = pd.DataFrame(klines, columns=['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time',
-                                           'Quote asset volume', 'Number of trades', 'Taker buy base asset volume',
-                                           'Taker buy quote asset volume', 'Ignore'])
+        endpoint = 'https://fapi.binance.com/fapi/v1/klines'
+        params = {'symbol': symbol, 'interval': interval, 'limit': limit}
+        response = requests.get(endpoint, params=params)
+        data = response.json()
+        df = pd.DataFrame(data, columns=['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time',
+                                         'Quote asset volume', 'Number of trades', 'Taker buy base asset volume',
+                                         'Taker buy quote asset volume', 'Ignore'])
         df[['Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'Quote asset volume',
             'Taker buy base asset volume', 'Taker buy quote asset volume']] = df[
             ['Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'Quote asset volume',
              'Taker buy base asset volume', 'Taker buy quote asset volume']].apply(pd.to_numeric)
-        
-        # Convert time to IST and remove timezone offset
-        df['Open time'] = pd.to_datetime(df['Open time'], unit='ms').dt.tz_localize(pytz.utc).dt.tz_convert('Asia/Kolkata').dt.tz_localize(None)
-        df['Close time'] = pd.to_datetime(df['Close time'], unit='ms').dt.tz_localize(pytz.utc).dt.tz_convert('Asia/Kolkata').dt.tz_localize(None)
-        
-        df = df.sort_values('Open time', ascending=False)
-        df = df.reindex(columns=['Open time', 'Open', 'Close', 'High', 'Low', 'Number of trades', 'Volume',
-                                 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Quote asset volume'])
 
+        # Convert timestamps to local timezone (IST)
+        ist_tz = pytz.timezone("Asia/Kolkata")
+        df['Open time'] = pd.to_datetime(df['Open time'], unit='ms', utc=True).dt.tz_convert(ist_tz)
+
+        # Separate date and time columns
+        df['Date'] = df['Open time'].dt.date
+        df['Time'] = df['Open time'].dt.time
+
+        # Remove "+05:30" offset from time
+        df['Time'] = df['Time'].apply(lambda x: x.replace(tzinfo=None))
+
+        df = df.sort_values('Open time', ascending=False)
+        df = df.reindex(columns=['Date', 'Time', 'Open', 'Close', 'High', 'Low', 'Number of trades', 'Volume',
+                                 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Quote asset volume'])
         return df
 
     def run(self):
@@ -38,8 +42,7 @@ class FuturesApp:
         st.markdown('---')
 
         st.markdown('<h1 class="header-title">Get the Pair Data for Data Modelling</h1>', unsafe_allow_html=True)
-        st.markdown('<p class="header-subtitle">Select the time frame and pair you are interested in:</p>',
-                    unsafe_allow_html=True)
+        st.markdown('<p class="header-subtitle">Select the time frame and pair you are interested in:</p>', unsafe_allow_html=True)
 
         tf = st.radio('Time frame for trading', ('1m', '3m', '5m', '30m', '1h'))
         pr = st.text_input('Which pair are you interested in?', autocomplete='off', key='pair_input')
@@ -58,18 +61,16 @@ class FuturesApp:
                 st.dataframe(df.head(limit), height=400)
 
                 csv = df.to_csv(index=False)
-                st.markdown(
-                    '<div class="download-csv"><a href="data:text/csv;base64,{0}" download="{1}.csv">Download CSV</a></div>'.format(
-                        base64.b64encode(csv.encode()).decode(), pr), unsafe_allow_html=True)
+                st.markdown('<div class="download-csv"><a href="data:text/csv;base64,{0}" download="{1}.csv">Download CSV</a></div>'.format(
+                    base64.b64encode(csv.encode()).decode(), pr), unsafe_allow_html=True)
 
                 st.markdown('---')
-
-                # st.subheader('BTCUSDT Prices')
 
             else:
                 st.warning('No data available for the selected pair and time frame.')
         except Exception as e:
             st.error(f'Error occurred: {str(e)}')
+
 
 if __name__ == '__main__':
     bot = FuturesApp()
